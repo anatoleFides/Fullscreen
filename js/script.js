17ac0ca708ac901868c1a462d30c20f57a52d44b
Anatolii Bedilo <fides2013ml@gmail.com>
2021-08-16 23:03:21 +0300

    deleted js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,40 @@
-$ document.ready(function(){
-	var
-		screen = 0,
-		container = $('.wrapper'),
-		pages = $('.fullscreen'),
-		inscroll = false;
-	$('.fullscreen:first-child').addClass('active');
-	$('body').on('mousewhell'),function(event){
-		var
-			activePage = pages.filter('.active');
-		if (!inscroll){
-			inscroll = true;
-			if(event.delayY>0){
-				if(activePage.prev().length){
-					screen--;
-				}
-			} else {
-				if(activePage.next().length){
-					screen++;
-				}
-			}
-		}
-		var
-			position = (-screen*100) + '%';
-		pages.eq(screen).addClass('active').siblings().removeClass('active');
-		container.css('top',position);
-		setTimeout(function(){
-			inscroll = false;
-		},1300);
-	}
-});
+// $(document).ready(function(){
+
+// 	var
+// 		screen = 0,
+// 		container = $('.wrapper'),
+// 		pages = $('.fullscreen'),
+// 		inscroll = false;
+
+// 	$('page:first-child').addClass('active');
+
+// 	$('body').on('mousewheel', function(event){
+		
+// 		var
+// 			activePage = pages.filter('.active');
+
+// 		if (!inscroll) {
+// 			inscroll = true;
+// 			if (event.deltaY > 0){
+				
+// 				if (activePage.prev().length){
+// 					screen--;
+// 				}
+// 			} else {
+// 				if (activePage.next().length){
+// 					screen++;
+// 				}
+				
+// 			}
+// 		}
+// 		var
+// 			position = (-screen*100) + '%';
+
+// 		pages.eq(screen).addClass('active').siblings().removeClass('active');
+// 		container.css('top', position);
+// 		setTimeout(function(){
+// 			inscroll = false;
+// 		}, 1300);
+
+// 	});
+// });
